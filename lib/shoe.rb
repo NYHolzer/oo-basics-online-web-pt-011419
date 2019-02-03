97eed1256add8b039285cabe9051b594ceb494fe
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-03 02:13:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,25 +1,9 @@
 class Shoe
-  attr_accessor :color :size
+  attr_accessor :color :size :material
   attr_reader :brand
   
   def initialize(brand)
     @brand = brand 
-  end
-  
-  def color=(color)
-    @color = color
-  end
-  
-  def color
-    @color 
-  end
-  
-  def size=(size)
-    @size = size
-  end 
-  
-  def size 
-    @size
   end
   
 end 
